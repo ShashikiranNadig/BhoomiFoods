26aa5656c1291abc6e669b03ad82b7ac610cc8b3
ShashikiranNadig <31413301+ShashikiranNadig@users.noreply.github.com>
2025-10-27 22:01:52 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['images.unsplash.com', 'pixabay.com', 'images.pexels.com'],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
   },
 }
 
